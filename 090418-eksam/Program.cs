17ae6680c139053ced0c1fd4c52e529f8b61ae1b
glaanemets <32923795+glaanemets@users.noreply.github.com>
2018-04-09 09:36:53 +0300

    Add project files.

---
+++ 090418-eksam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _090418_eksam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
